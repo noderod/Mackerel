3000076d49762a8fc9a66489389cd3e5f9db4f30
Carlos Red <carlos.red@utexas.edu>
2018-01-04 20:51:43 +0100

    Person mode almost finished

--- jurel.py
+++ jurel.py
@@ -6,8 +6,31 @@ to the user's slides number and returns it to the main program.
 """
 
 import wikipedia
-import sumy
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 import os
+import gc
+
+
+
+# Checks if any of the following elements are present in a list, True if correct
+# ltc (list): List to check
+# el2c (list): elements to be checked in the list
+
+def anyelem(ltc, el2c):
+
+    for thisel in el2c:
+        if thisel in ltc:
+            return True
+    else:
+        return False
+
+
+
+################################################################################
+# ACTUAL DEVELOPMENT
+################################################################################
 
 # No actual functions being developed, experimental so far
 
@@ -16,49 +39,97 @@ postopic = 'Augustus'
 wiktop = wikipedia.page(postopic).content
 
 
-# Nature of the article
-topicart = ['person', 'place', 'art', 'common object', 'lifeform', 'religion','ideology']
-
-
 sumtop = wikipedia.summary(postopic, sentences = 1)
-print(sumtop)
 
 # List of words which indicates a certain topic
 
 artictop = {
     'person' : ['activist', 'actor', 'artist', 'bussinessman', 'CEO', 'designer',
-                'emperor', 'entrepeneur', 'founder', 'inventor', 'king', 'polymath',
-                'producer'],
-    'place' : ['country', 'state'],
-    'art' : [],
+                'doctor', 'emperor', 'entrepeneur', 'inventor', 'king', 'photographer',
+                'politician', 'polymath', 'president', 'producer', 'prophet'],
+    'place' : ['capital', 'city', 'country', 'municipality', 'prefecture', 'state',
+              'town', 'village'],
+    'art' : ['statue', 'painting', 'film', 'movie'],
     'common object' : [],
-    'lifeform' : ['species'],
-    'religion' : ['relig'],
-    'ideology' : []
+    'lifeform' : ['animal', 'bacteria', 'organism', 'plant', 'protozoo', 'species'],
+    'religion' : ['relig', 'theism'],
+    'ideology' : ['politic']
 }
 
 
 # Determines the topic of the article
+notfound = True
 
 for postt in artictop.keys():
-    if postt in sumtop.lower():
-        topic = postt
-        break
 
     for identifier in artictop[postt]:
         if identifier in sumtop.lower():
             topic = postt
+            notfound = False
             break
+
+    if notfound == False:
+        break
 
 
 print(topic)
 
-# Obtains the information in parenthesis
-repbar = sumtop.replace('(', '|||').replace(')', '|||').split('|||')[1]
+
+# If the topic is a person, then there are several things needed
+if topic == 'person':
+    occupations = []
+    for posocc in artictop['person']:
+        if posocc in sumtop:
+            occupations.append(posocc)
+
+    # Obtains the birth-death dates
+    repbar = sumtop.replace('(', '|||').replace(')', '|||').split('|||')
+    for elem in repbar:
+        if ';' in elem:
+            birdea = elem
+            break
+
+    birdea = birdea.split(';')[1]
+    birdea = birdea.split('–') # This is not a keyboard key
+    if len(birdea) == 1:
+        # Still alive
+        Bdat = birdea[0]
+        Ddat = 'Alive'
+    else:
+        Bdat = birdea[0]
+        Ddat = birdea[1]
 
 
+# Dumps all the content into a text file, except the last part
+tobeus = wiktop.split('== See also ==')
 
-# Finds the name of the sections
+
+# Skips the titles
+actualtext = tobeus[0].replace('=', '\n')
+gc.collect()
 
 
-# Identifies if the URL is about a person, place, or thing
+# Transforms the unicode into ascii to avoid future problems
+tobeus_enc = actualtext.encode('ascii', errors='ignore').decode()
+
+with open('Tempfile___.txt', 'w') as lolfil:
+    lolfil.write(tobeus_enc)
+
+
+thefile = "Tempfile___.txt" #name of the plain-text file
+parser = PlaintextParser.from_file(thefile, Tokenizer("english"))
+summarizer = LexRankSummarizer()
+
+summary = summarizer(parser.document, 15) #Summarize the document with 5 sentences
+
+print(sumtop)
+
+for sentence in summary:
+    # Skips contents already looked over and possible titles
+    if (anyelem(str(sentence), ['born', 'died in']) == True) or (len(str(sentence).split()) < 5):
+        continue
+
+    print(sentence)
+
+# summarizes the text file using the number of slides provided by the user
+os.remove('Tempfile___.txt') # Removes the file
